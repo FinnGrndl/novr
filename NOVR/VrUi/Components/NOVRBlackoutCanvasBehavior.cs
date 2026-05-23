494b852935ede8c57f1f11d75072deff1072303b
deltawing <williamtetzlaff123@gmail.com>
2026-05-23 14:22:46 +1200

    Blackout canvas really fixed this time

--- NOVR/VrUi/Components/NOVRBlackoutCanvasBehavior.cs
+++ NOVR/VrUi/Components/NOVRBlackoutCanvasBehavior.cs
@@ -5,19 +5,25 @@ namespace NOVR.VrUi.SpecialBehavior;
 
 public class NOVRBlackoutCanvasBehavior : MonoBehaviour
 {
+    
     protected virtual void Awake()
     {
         var canvas = gameObject.GetComponent<Canvas>();
         if (canvas == null) throw new Exception($"{typeof(NOVRBlackoutCanvasBehavior)} attached to {typeof(GameObject)} without {typeof(Canvas)} component.");
         
         ApplyVrUiLayerRecursive(canvas.transform);
-        canvas.transform.localPosition = canvas.transform.localPosition with { x = -500, y = -500, z = 0 };
-        canvas.transform.localScale = Vector3.one * 100;
-        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.renderMode = RenderMode.WorldSpace;
         canvas.worldCamera = APIBus.CockpitHudCamera;
         canvas.planeDistance = 1f;
     }
-    
+
+    private void Update()
+    {
+        transform.rotation = APIBus.CockpitHudCamera.transform.rotation;
+        transform.position = APIBus.CockpitHudCamera.transform.position
+                             + APIBus.CockpitHudCamera.transform.forward;
+    }
+
     private static void ApplyVrUiLayerRecursive(Transform root)
     {
         LayerHelper.SetLayerRecursive(root, LayerHelper.GetVrUiLayer());
